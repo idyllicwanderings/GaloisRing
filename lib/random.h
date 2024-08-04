a080d9c11dbb8f2193d2ec21e4894c4e991971ae
Xiyue <suxiyue18@gmail.com>
2024-08-04 21:50:09 +0200

    random oracle edited but to be tested

--- lib/random.h
+++ lib/random.h
@@ -1,92 +1,61 @@
-/*
-Copyright (c) 2017, The University of Bristol, Senate House, Tyndall Avenue, Bristol, BS8 1TH, United Kingdom.
-Copyright (c) 2021, COSIC-KU Leuven, Kasteelpark Arenberg 10, bus 2452, B-3001 Leuven-Heverlee, Belgium.
+// Random Oracle
 
-All rights reserved
-*/
-#ifndef _random
-#define _random
+// give an input, pass to the hash function, then to the PRG , then store a pointer to PRG, 
+// then return the truncation of a PRG output
 
-#include "aes.h"
+#ifndef RANDOM_H
+#define RANDOM_H
 
-#include <vector>
-using namespace std;
+#include <iostream>
+#include <string>
+#include <fstream>
 
-#define PIPELINES 8
+#include "SimpleFIPS202.h"
+#include "KeccakPRG.h"
 
-// If SEED_SIZE != AES_BLK_SIZE all sorts of things will no work
-#define SEED_SIZE AES_BLK_SIZE
+#define SEED_BYTE_LEN 16
+#define SHA3_WIDTH 256
+#define OUT_BYTE_LEN 256
 
-#define RAND_SIZE (PIPELINES * AES_BLK_SIZE)
+class random {
 
-/* This basically defines a randomness expander, if using
- * as a real PRG on an input stream you should first collapse
- * the input stream down to a SEED, say via CBC-MAC (under 0 key)
- * or via a hash
- */
+    uint8_t out_[OUT_BYTE_LEN];
+    uint64_t p_;
 
-// __attribute__ is needed to get the sse instructions to avoid
-//  seg faulting.
+    void gen_random_bytes() {
+        // SHA3之后就是shake256
+        uint8_t seed_[SEED_BYTE_LEN];
+        uint8_t hash_[#SHA3_WIDTH / 8];
+        seed(seed_, SEED_BYTE_LEN);
+        int ret = SHA3_#SHA3_WIDTH(hash_, seed_, SEED_BYTE_LEN); 
+        assert(ret == 0);
+        ret = SHAKE256(out_, OUT_BYTE_LEN, hash_, #SHA3_WIDTH / 8);
+        static_assert(ret == 0);
+        p_ = 0;
+    }
 
-class PRNG
-{
-  uint8_t seed[SEED_SIZE];
-  uint8_t state[RAND_SIZE] __attribute__((aligned(16)));
-  uint8_t random[RAND_SIZE] __attribute__((aligned(16)));
+    void get_bytes(uint8_t *out, unsigned int byte_len) {
+        if (p_ + byte_len <= OUT_BYTE_LEN) {
+            std::memcpy(out, out_ + p_, byte_len);
+            p_ += byte_len;
+        } else {
+        // TODO: Handle the error if requested length exceeds the available length
+        }
+    }
 
-  bool useC;
+    void get_bits(uint8_t *out, unsigned int bit_len) {
+        unsigned int byte_len = (bit_len + 7) / 8;
+        get_bytes(out, byte_len);
+        out[byte_len - 1] &= (1 << (bit_len % 8)) - 1;
+    }
 
-  // Two types of key schedule for the different implementations
-  // of AES
-  uint KeyScheduleC[44];
-  uint8_t KeySchedule[176] __attribute__((aligned(16)));
+    void seed(uint8_t *out, unsigned int byte_len) {
+        std::ifstream urandom_stream("/dev/u/random", std::ios::in | std::ios::binary);
+        if (!urandom_stream) std::cout << "cannot open dev/u/random " << std::endl;
+        urandom_stream.read(reinterpret_cast<char*>(out), byte_len);
+        if (!urandom_stream) std::cout << "error reading dev/u/random" << std::endl;
+    }
 
-  int cnt; // How many bytes of the current random value have been used
-
-  void hash(); // Hashes state to random and sets cnt=0
-  void next();
-
-public:
-  PRNG();
-
-  // For debugging
-  void print_state() const;
-
-  // Set seed from dev/random and Init the generator
-  //   - Takes the thread number to ensure divergence
-  //     in different threads. Recommended by Tancrede
-  //     Lepoint
-  void ReSeed(int thread);
-
-  // This is the same but without the CBC-MAC
-  //   Assumes input is randomness not controlled
-  //   by an adversary
-  void SetSeedFromRandom(unsigned char *);
-
-  // Get a seed out of G
-  void SetSeed(PRNG &G);
-
-  // Take seed and initialize the PRNG
-  void InitSeed();
-
-  double get_double();
-  unsigned char get_uchar();
-  // Gets 32 bits
-  unsigned int get_uint();
-  // Gets 64 bits
-  int64_t get_word();
-  __m128i get_doubleword();
-
-  const uint8_t *get_seed() const
-  {
-    return seed;
-  }
-
-  /* Returns len random bytes */
-  void get_random_bytes(uint8_t *v, int len);
-  /* Same but with len=v.size() */
-  void get_random_bytes(vector<uint8_t> &v);
 };
 
 #endif
-
